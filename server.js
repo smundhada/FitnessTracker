c7897697c4092d7fd3649e13ad70814248ee0ea6
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-11-19 17:14:28 -0600

    Adding in the new file data 2

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
-//Getting all the required 
+//Getting all the required modules 
 const express = require("express");
 const mongoose = require("mongoose");
 
+//Setting up the ports
 const PORT = process.env.PORT || 3000;
 
+//App connections 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
@@ -11,11 +13,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+//Routes to the files
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+//Database Connection getting created
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {useNewUrlParser: true,});
 
+//Listening to which port
 app.listen(PORT, function () {
   console.log("Listening to port: " + PORT);
 });
